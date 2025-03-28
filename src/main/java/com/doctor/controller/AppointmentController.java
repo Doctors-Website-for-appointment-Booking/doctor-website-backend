fb8503364934b56350b873e4f6b672f7cd13a97a
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-28 18:18:42 +0530

    Update AppointmentController.java

--- src/main/java/com/doctor/controller/AppointmentController.java
+++ src/main/java/com/doctor/controller/AppointmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/appointments")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://doctor-website-backend-production.up.railway.app/")
 public class AppointmentController {
     private final AppointmentService appointmentService;
 
