bcdef25ead02f676298a4c106a8ba91e98c6a68e
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-04-01 14:35:25 +0530

    Update WebConfig.java

--- src/main/java/com/doctor/config/WebConfig.java
+++ src/main/java/com/doctor/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://shyamhomeopathy.netlify.app")
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                 .allowedHeaders("*")
+                .exposedHeaders("Authorization")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
