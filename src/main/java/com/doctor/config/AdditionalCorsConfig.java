e5a127f56e4caf4a19c7bad3e3b36b8b2f0b09dd
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-04-01 14:36:38 +0530

    Create AdditionalCorsConfig.java

---
+++ src/main/java/com/doctor/config/AdditionalCorsConfig.java
@@ -0,0 +1,22 @@
+package com.doctor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class AdditionalCorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://shyamhomeopathy.netlify.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
