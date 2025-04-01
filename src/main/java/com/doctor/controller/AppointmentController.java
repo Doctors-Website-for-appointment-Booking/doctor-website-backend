0618a49b57274c8cf46a0ef70f0688a3732ba266
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-04-01 19:17:50 +0530

    Update AppointmentController.java

--- src/main/java/com/doctor/controller/AppointmentController.java
+++ src/main/java/com/doctor/controller/AppointmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/appointments")
-//@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://shyamhomeopathy.netlify.app")
 public class AppointmentController {
     private final AppointmentService appointmentService;
 
